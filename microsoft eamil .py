b8924ffd4d6cb6fbc00c9dbcc813f03741ddc49d
youness gawgaw334 <113532154+younessgawgawpyja@users.noreply.github.com>
2022-09-18 04:23:38 +0300

    Add files via upload

---
+++ microsoft eamil .py
@@ -0,0 +1,23 @@
+# this is an variables 
+gmail_name = input("enter your name  :")
+company_name = input ("your email from micrsoft or google :")
+
+#this function for random number
+import random
+number = random.randint(1000000,9999999)
+# this function for a simple loop for printing the variable in a lot of times
+x = "WAAAAAAIIIIIIITTTTTT\n"
+z = 25
+
+print(z * x)
+print("your code is :",number)
+print("Hi"+gmail_name +"@gmail.com,")
+
+print("We received your request for a single-use code to use with your "+company_name+ " account.")
+print("Your single-use code is: ",number)
+
+print("If you didn't request this code, you can safely ignore this email. Someone else might have typed your email address by mistake.")
+
+print("Thanks,")
+
+print("The "+gmail_name+" account team")
